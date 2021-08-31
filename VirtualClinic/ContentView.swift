c5130f5a5057ca3387cb89f742461333877d4a3b
MackleyMag <mackleymagalhaess@hotmail.com>
2021-08-30 22:28:31 -0300

    Initial Commit

---
+++ VirtualClinic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VirtualClinic
+//
+//  Created by Mackley Magalhães da Silva on 30/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VirtualClinic/VirtualClinicApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualClinicApp.swift
+//  VirtualClinic
+//
+//  Created by Mackley Magalhães da Silva on 30/08/21.
+//
+
+import SwiftUI
+
+@main
+struct VirtualClinicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
